6f34b979f14b65a528e67a92c78cb9de7ee678dc
Giorgio Germano <giorgio.germano.ravina@gmail.com>
2024-05-07 10:21:00 +0200

    Refactor code for fitting linear functions to data

--- a.C
+++
@@ -1,9 +0,0 @@
-#include "TMath.h"
-#include "TF1.h"
-
-void a()
-{
-    TF1 *fitTweeter = new TF1("fT", "TMath::ATan(1/(x*[0]*[1]))", 8000, 12000);
-    fitTweeter->SetParameters(26.1e-9, 560.);
-    fitTweeter->Draw();
-}
